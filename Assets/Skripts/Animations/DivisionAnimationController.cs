f7da6bba44e45e5423acd29dfd1012de560ba16b
misha <mishagjxn@mail.ru>
2025-07-05 13:42:14 +0500

    Сделать уменьшение размера дискретным

--- Assets/Skripts/Animations/DivisionAnimationController.cs
+++ Assets/Skripts/Animations/DivisionAnimationController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
@@ -32,7 +33,9 @@ public class DivisionAnimationController : MonoBehaviour, iAnimationController
     {
         if (buttleUnit != null)
         {
+            // Регулируем размер
             var k = buttleUnit.getUnitCount() / buttleUnit.GetMaxUnitCount();  // от 0 до 1
+            k = (float)Math.Round(k, 1);  // Округляем до первого знака после запятой
             var scale = 0.5f + k / 2;
             transform.localScale = new Vector3(scale, scale, scale);
         }
